9ff00ff1d1062e02b242f7e37156fdc189125c71
Ben Sivoravong <bensivo@gmail.com>
2024-09-16 21:35:33 -0500

    mnist: added gaussian and bernoulli, on raw and processed data

--- mnist/main.py
+++ mnist/main.py
@@ -1,5 +1,5 @@
 import numpy as np
-from sklearn.naive_bayes import GaussianNB
+from sklearn.naive_bayes import GaussianNB, BernoulliNB
 
 from operations.load_mnist import load_mnist
 from operations.im_preprocess import im_preprocess
@@ -45,23 +45,45 @@ for i in range(eval_data_raw.shape[0]):
     )
 
 
-# Flatten the processed images into 1-dimensional feature vectors
-print('Building feature vectors')
-training_data_flattened = training_data_processed.reshape(training_data_processed.shape[0], -1)
-eval_data_flattened = eval_data_processed.reshape(eval_data_processed.shape[0], -1)
+def train_and_eval(nb_distribution, training_data, training_labels, eval_data, eval_labels):
+    """
+    Train a Naive Bayes classifier on the training data and evaluate it on the eval data.
+
+    Params:
+        nb_distribution: 'gaussian' or 'bernoulli'
+        training_data: [N,H,W] numpy array of training images
+        training_labels: [N] numpy array of training image labels
+        eval_data: [N,H,W] numpy array of eval images
+        eval_labels: [N] numpy array of eval image labels
+
+    Returns:
+        accuracy: the accuracy of the classifier on the eval data
+    """
+    # Flatten the original images into 1-dimensional feature vectors
+    training_data_flattened = training_data.reshape(training_data.shape[0], -1)
+    eval_data_flattened = eval_data.reshape(eval_data.shape[0], -1)
+
+    if nb_distribution == 'gaussian':
+        classifier = GaussianNB()
+    elif nb_distribution == 'bernoulli':
+        classifier = BernoulliNB()
+    else:
+        raise ValueError(f'Invalid Naive Bayes distribution: {nb_distribution}')
+
+    classifier.fit(training_data_flattened, training_labels)
+    predictions = classifier.predict(eval_data_flattened)
+    accuracy = np.count_nonzero(predictions == eval_labels) / eval_labels.shape[0]
+    return accuracy
 
 
-# Fit a Naive Bayes classifier on the training data
-print('Fitting Naive Bayes classifier')
-classifier = GaussianNB()
-classifier.fit(training_data_flattened, training_labels_raw)
+accuracy_gaussian_raw = train_and_eval('gaussian', training_data_raw, training_labels_raw, eval_data_raw, eval_labels_raw)
+accuracy_bernoulli_raw = train_and_eval('bernoulli', training_data_raw, training_labels_raw, eval_data_raw, eval_labels_raw)
+accuracy_gaussian_processed = train_and_eval('gaussian', training_data_processed, training_labels_raw, eval_data_processed, eval_labels_raw)
+accuracy_bernoulli_processed = train_and_eval('bernoulli', training_data_processed, training_labels_raw, eval_data_processed, eval_labels_raw)
+
+print('Accuracy (Gaussian, raw):', accuracy_gaussian_raw)
+print('Accuracy (Bernoulli, raw):', accuracy_bernoulli_raw)
+print('Accuracy (Gaussian, processed):', accuracy_gaussian_processed)
+print('Accuracy (Bernoulli, processed):', accuracy_bernoulli_processed)
 
 
-# Predict the labels of the evaluation data
-print('Making predictions')
-predictions = classifier.predict(eval_data_flattened)
-
-
-# Calculate accuracy
-accuracy = np.mean(predictions == eval_labels_raw)
-print(f'Accuracy: {accuracy}')
