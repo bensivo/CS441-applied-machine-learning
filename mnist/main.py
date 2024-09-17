3fea91d76a1d4a7693ef9e82192eb2213d949c0b
Ben Sivoravong <bensivo@gmail.com>
2024-09-16 22:18:35 -0500

    Added random forest

--- mnist/main.py
+++ mnist/main.py
@@ -1,5 +1,6 @@
 import numpy as np
 from sklearn.naive_bayes import GaussianNB, BernoulliNB
+from sklearn.ensemble import RandomForestClassifier
 
 from operations.load_mnist import load_mnist
 from operations.im_preprocess import im_preprocess
@@ -45,7 +46,7 @@ for i in range(eval_data_raw.shape[0]):
     )
 
 
-def train_and_eval(nb_distribution, training_data, training_labels, eval_data, eval_labels):
+def train_and_eval(model_type, training_data, training_labels, eval_data, eval_labels):
     """
     Train a Naive Bayes classifier on the training data and evaluate it on the eval data.
 
@@ -63,12 +64,18 @@ def train_and_eval(nb_distribution, training_data, training_labels, eval_data, e
     training_data_flattened = training_data.reshape(training_data.shape[0], -1)
     eval_data_flattened = eval_data.reshape(eval_data.shape[0], -1)
 
-    if nb_distribution == 'gaussian':
+    if model_type == 'gaussian':
         classifier = GaussianNB()
-    elif nb_distribution == 'bernoulli':
+    elif model_type == 'bernoulli':
         classifier = BernoulliNB()
+    elif model_type == 'random_forest':
+        classifier = RandomForestClassifier(
+            n_estimators=100,  # These params are very low, but we're optimizing for speed, not performance
+            max_depth=10,
+            random_state=12345
+        )
     else:
-        raise ValueError(f'Invalid Naive Bayes distribution: {nb_distribution}')
+        raise ValueError(f'Invalid model type: {model_type}')
 
     classifier.fit(training_data_flattened, training_labels)
     predictions = classifier.predict(eval_data_flattened)
@@ -78,12 +85,16 @@ def train_and_eval(nb_distribution, training_data, training_labels, eval_data, e
 
 accuracy_gaussian_raw = train_and_eval('gaussian', training_data_raw, training_labels_raw, eval_data_raw, eval_labels_raw)
 accuracy_bernoulli_raw = train_and_eval('bernoulli', training_data_raw, training_labels_raw, eval_data_raw, eval_labels_raw)
+accuracy_random_forest_raw = train_and_eval('random_forest', training_data_raw, training_labels_raw, eval_data_raw, eval_labels_raw)
 accuracy_gaussian_processed = train_and_eval('gaussian', training_data_processed, training_labels_raw, eval_data_processed, eval_labels_raw)
 accuracy_bernoulli_processed = train_and_eval('bernoulli', training_data_processed, training_labels_raw, eval_data_processed, eval_labels_raw)
+accuracy_random_forest_processed = train_and_eval('random_forest', training_data_processed, training_labels_raw, eval_data_processed, eval_labels_raw)
 
 print('Accuracy (Gaussian, raw):', accuracy_gaussian_raw)
 print('Accuracy (Bernoulli, raw):', accuracy_bernoulli_raw)
+print('Accuracy (Random Forest, raw):', accuracy_random_forest_raw)
 print('Accuracy (Gaussian, processed):', accuracy_gaussian_processed)
 print('Accuracy (Bernoulli, processed):', accuracy_bernoulli_processed)
+print('Accuracy (Random Forest, processed):', accuracy_random_forest_processed)
 
 
